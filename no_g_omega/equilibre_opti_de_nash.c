7d715efc46f5e156e586b99274e5606b4a17d75c
Maëlys Eymond <135099544+Maelys-e@users.noreply.github.com>
2025-07-25 13:15:59 +0200

    Update equilibre_opti_de_nash.c

--- no_g_omega/equilibre_opti_de_nash.c
+++ no_g_omega/equilibre_opti_de_nash.c
@@ -54,20 +54,16 @@ struct CPtuple
 
 struct CPtuple maxrcp(float p, float Ci[], float Qfi[], struct Market market, struct CDN cdn, int i) 
 {
-  //printf("request price = %f ", request_price);
     double R;
     float pk = 0;
     double Max = 0;
     float pmax = 0;
-    //printf("tour1 - %d\n", i);
     for (int k = 0; k < (int)(prixMax * 10); k++)
     {
         pk = round((0.1 + k*0.1) * 10)/10;
         R = rcp(pk, p, Ci, Qfi, market, cdn, i);
-        //printf("REVENU : %f", R);
         if (R > Max)
         {
-          //printf("realR : %f", R);
             pmax = pk;
             Max = R;
         }
@@ -78,7 +74,6 @@ struct CPtuple maxrcp(float p, float Ci[], float Qfi[], struct Market market, st
     {
         pk = round((start + k*0.01) * 100)/100;
         R = rcp(pk, p, Ci, Qfi, market, cdn, i);
-        //printf("REVENU : %f", R);
         if (R > Max)
         {
             pmax = pk;
@@ -91,7 +86,6 @@ struct CPtuple maxrcp(float p, float Ci[], float Qfi[], struct Market market, st
     {
         pk = round((start + k*0.001) * 1000)/1000;
         R = rcp(pk, p, Ci, Qfi, market, cdn, i);
-        //printf("REVENU : %f", R);
         if (R > Max)
         {
             pmax = pk;
@@ -104,7 +98,6 @@ struct CPtuple maxrcp(float p, float Ci[], float Qfi[], struct Market market, st
     {
         pk = round((start + k*0.0001) * 10000)/10000;
         R = rcp(pk, p, Ci, Qfi, market, cdn, i);
-        //printf("REVENU : %f", R);
         if (R > Max)
         {
             pmax = pk;
@@ -115,9 +108,6 @@ struct CPtuple maxrcp(float p, float Ci[], float Qfi[], struct Market market, st
     struct CPtuple tab;
     tab.pk = pmax;
     tab.R = Max;
-    //float* tab = (float*)malloc(2*sizeof(float));
-    //*tab = {pk, R};
-    //printf("real p = %f & revenu = %f / ", pmax, Max);
     return(tab);
 }
 
@@ -133,14 +123,9 @@ struct Nash
     
 struct Nash nash2(float C1, float C2, float Q1, float Q2, struct Market market, struct CDN cdn)
 {
-    //printf("nash2\n");
     float p1 = 1;
-    //float p1_last = 1;
-    //float p1_before;
     float p1_before[20] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     float p2 = 1;
-    //float p2_last = 1;
-    //float p2_before;
     float p2_before[20] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     struct CPtuple tab;
     float Ci[3] = {0, C1, C2};
@@ -159,24 +144,16 @@ struct Nash nash2(float C1, float C2, float Q1, float Q2, struct Market market, 
           p1_before[i] = p1_before[i-1];
         }
         p1_before[0] = p1;
-        
-        //p1_before = p1_last;
-        //p2_before = p2_last;
-        //p1_last = p1;
-        //p2_last = p2;
-        //printf("TOUR %d\n", i+1);
-        //printf("(p1, p2) = (%f,%f)\n", p1, p2);
+      
         tab = maxrcp(p1, Ci, Qfi, market, cdn, 2);
         p2 = tab.pk;
         tab = maxrcp(p2, Ci, Qfi, market, cdn, 1);
         p1 = tab.pk;
-        //printf("(p1, p2) = (%f,%f) - C = %f - p = %f\n", p1, p2, Ci[1], cdn.request_price);
         
         for (int j = 0; j < 20; j++)
         {
           if (round(fmaxf(p1 - p1_before[j], p1_before[j]-p1)*1000)/1000 < 0.01 && round(fmaxf(p2 - p2_before[j], p2_before[j]-p2)*1000)/1000 < 0.01) // test au centième près
           {
-            //printf("%d\n", j);
             n = 0;
             S = 0;
             for (int k = 0; k < j+1; k++)
@@ -193,16 +170,8 @@ struct Nash nash2(float C1, float C2, float Q1, float Q2, struct Market market, 
             break;
           }
         }
-        
-        //if ((round(fmaxf(p1 - p1_last, p1_last-p1)*10000)/10000 < 0.002 && round(fmaxf(p2 - p2_last, p2_last-p2)*10000)/10000 < 0.002)||(round(fmaxf(p1 - p1_before, p1_before-p1)*10000)/10000 < 0.002 && round(fmaxf(p2 - p2_before, p2_before-p2)*10000)/10000 < 0.002))
-        //{
-          //n = 0;
-        //}
     }
-    //print("nash", p1, p2)
     struct Nash values;
-    //printf("(p1,p2) = (%f,%f)", p1, p2);
-    //printf("(p1, p2) = (%f,%f) - C = %f - p = %f\n", p1, p2, Ci[1], cdn.request_price);
     values.rCP1 = rcp(p1, p2, Ci, Qfi, market, cdn, 1);
     values.rCP2 = rcp(p2, p1, Ci, Qfi, market, cdn, 2);
     float SR;
@@ -223,7 +192,6 @@ struct Nash nash2(float C1, float C2, float Q1, float Q2, struct Market market, 
         values.has_2 = false;
         values.value_2 = 0;
     }
-    //printf("before (%d, %d)", values.has_1, values.has_2);
     return values;
 }
     
@@ -262,9 +230,6 @@ int main()
         
     for (int i = 0; i < (int)(301); i++)
     {
-      //float pi = i*0.1;
-      //struct CPtuple tab1 = maxrcp(pi, Cis, Qfi, market, cdn, 1);
-      //struct CPtuple tab2 = maxrcp(pi, Cis, Qfi, market, cdn, 2);
       float pa = i*0.1;
       float pb = 12;
       float R = rcp(pa, pb, Cir, Qfi, market, cdn, 1);
