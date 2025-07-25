171dc9b15fbfa497da11026ae2050fb558b1a30d
Maëlys Eymond <135099544+Maelys-e@users.noreply.github.com>
2025-07-25 13:12:16 +0200

    Update tracer_courbes.py

--- no_g_1_CP/tracer_courbes.py
+++ no_g_1_CP/tracer_courbes.py
@@ -28,29 +28,7 @@ if __name__ == "__main__" :
         print("ERREUR Ouverture fichier " + nomFic)
         sys.exit()
     M = np.loadtxt(nomFic)
-    #plt.scatter(M[:,0], M[:,1])
-    #print(M)
     N = M.T
-    
-    #cdn_prices = N[0]
-    #print("prices :", cdn_prices)
-    """
-    cdn_revenu = N[1]
-    c1 = N[2]
-    c2 = N[3]
-    p1 = N[4]
-    p2 = N[5]
-    r1 = N[6]
-    r2 = N[7]
-    """
-    """
-    r1 = N[1]
-    r2 = N[2]
-    r3 = N[3]
-    r4 = N[4]
-    r5 = N[5]
-    r6 = N[6]
-    """
     
     a = N[0]
     R = N[1]
@@ -60,12 +38,6 @@ if __name__ == "__main__" :
     #m1 = N[5]
     #c1 = N[6]
     #q1 = N[7]
-    
-    #for i in range(len(a)) :
-    #    a[i] = 0.2*a[i] + 0.5
-    #for i in range(1, len(N[1])-1) :
-    #    if np.abs(N[1][i] - N[1][i-1]) > 1 and np.abs(N[1][i] - N[1][i+1]) > 1 :
-    #        N[1][i] = (N[1][i-1] + N[1][i+1])/2
     
 
     plt.style.use("ggplot")
@@ -90,52 +62,4 @@ if __name__ == "__main__" :
     tikzplotlib.save(output_file)
     plt.show()
     
-    """
-    plt.xlabel("Prix du CDN", fontdict={'family': 'serif'})
-    plt.ylabel("Capacité allouée", fontdict={'family': 'serif'})
-    plt.plot(cdn_prices, c1, label = "C1 (/9)", linewidth = 0.5, color = "#66FF33")
-    plt.plot(cdn_prices, c2, label = "C2 (/9)", linewidth = 0.5, color = "#FF9933")
-    plt.legend(fontsize = 10, loc = 1)
-    plt.ylim(-1, 10)
-    plt.xlim(0, 11.5)
-    plt.title("Répartition des capacités du CDN entre les CPs", fontdict={'family': 'serif'})
-    output_file = os.path.join(output_dir, f"ci_allocation_{timenow}.tex")
-    tikzplotlib.save(output_file)
-    plt.show()
-    
-    plt.title("Prix optimaux des CPs", fontdict={'family': 'serif'})
-    plt.xlabel("Prix du CDN", fontdict={'family': 'serif'})
-    plt.ylabel("Prix des CPs", fontdict={'family': 'serif'})
-    plt.plot(cdn_prices, p1, label = "prix CP1", linestyle = "--", color = "#006600")
-    plt.plot(cdn_prices, p2, label = "prix CP2", linestyle = "--", color = "#993300")
-    plt.legend(fontsize = 10, loc = 1)
-    plt.ylim(-1, 31)
-    plt.xlim(0, 11.5)
-    plt.show()
-    """
-    """
-    plt.title("Revenu d'un CDN seul", fontdict={'family': 'serif', 'size':15})
-    plt.plot(cdn_prices, r1, label = "G = 6", linewidth = 1, color = vert4) #99cc66
-    plt.plot(cdn_prices, r2, label = "G = 7", linewidth = 1, color = vert5) #cc9900
-    #plt.plot(cdn_prices, p21, label = "G = 0", linewidth = 1, color = vert4) #3388ee
-    plt.plot(cdn_prices, r3, label = "G = 8", linewidth = 1, color = vert7)
-    plt.plot(cdn_prices, r4, label = "G = 9", linewidth = 1, color = vert9)
-    #plt.plot(cdn_prices, r5, label = "G = 2", linewidth = 1, color = vert7) #99cc66
-    plt.plot(cdn_prices, r5, label = "G = 10", linewidth = 1, color = vert11) #cc9900
-    plt.plot(cdn_prices, r6, label = "G = 11", linewidth = 1, color = vert13) #3388ee
-    #plt.plot(cdn_prices, p23, label = "G = 4", linewidth = 1, color = vert10)
-    plt.legend(fontsize = 15, prop={'family': 'serif', 'size': 13}, loc = 1)
-    plt.ylim(-6, 25)
-    plt.xlim(0, 2)
-    plt.xlabel("Prix du CDN", fontdict={'family': 'serif', 'size': 13})
-    plt.ylabel("Revenu", fontdict={'family': 'serif', 'size': 13})
-    #plt.savefig("graph.png")
-    ax = plt.gca()
-    for label in ax.get_xticklabels():
-        label.set_fontproperties({'family': 'serif', 'size': 12})
-    for label in ax.get_yticklabels():
-        label.set_fontproperties({'family': 'serif', 'size': 12})
-    output_file = os.path.join(output_dir, f"cdn_revenu_{timenow}.tex")
-    tikzplotlib.save(output_file)
-    plt.show()
-    """
+  
